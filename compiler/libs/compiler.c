c8844dbde3db9d4e1f50ef6b165a6d698d573109
thenumber8equalsign <168995087+thenumber8equalsign@users.noreply.github.com>
2026-08-31 11:58:20 -0400

    i forgot null terminator

--- compiler/libs/compiler.c
+++ compiler/libs/compiler.c
@@ -77,7 +77,8 @@ static int handle_child_process(const char *pathname,
 		// we may be running on another system without the GNU assembler
 		// now try clang
 		executable_path = "clang";
-		execlp("clang", "clang", "-o", output_pathname, pathname);
+		execlp("clang", "clang", "-o", output_pathname, pathname,
+		       (char *)NULL);
 	}
 	return 0;
 }
